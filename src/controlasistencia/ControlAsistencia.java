9eceef1121e094271f3b4847bf4c7dc73404b377
skarlett01 <k.olivares27@alumnos.santotomas.cl>
2025-08-31 20:30:49 -0400

    waaaaa

--- src/controlasistencia/ControlAsistencia.java
+++ src/controlasistencia/ControlAsistencia.java
@@ -15,6 +15,8 @@ public class ControlAsistencia {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+           String bye= "bye";
     }
     
 }
